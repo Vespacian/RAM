5ffdbf2b1eebba51febedc9df584011c74a15d83
Eric <eric.kh.bae@gmail.com>
2024-07-31 15:14:27 -0700

    Setting up RAM repo

---
+++ SubCipherDnD.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubCipherDnD {
+
+  public Map<Character, Character> key = new HashMap<>(){{
+    put('a', '%');
+    put('b', '%');
+    put('c', '%');
+    put('d', '%');
+    put('e', '%');
+    put('f', '%');
+    put('g', '%');
+    put('h', '%');
+    put('i', '%');
+    put('j', '%');
+    put('k', '%');
+    put('l', '%');
+    put('m', '%');
+    put('n', '%');
+    put('o', '%');
+    put('p', '%');
+    put('q', '%');
+    put('r', '%');
+    put('s', '%');
+    put('t', '%');
+    put('u', '%');
+    put('v', '%');
+    put('w', '%');
+    put('x', '%');
+    put('y', '%');
+    put('z', '%');
+  }};
+
+  public static void main(String[] args) {
+    String message = "fcmhk\nabcdebfghied\nfjcdbkjkjlmnl";
+    
+    // loop through all possibilites of mappings
+    // convert the message using key
+    // save and analyze if valid words exist
+    // if valid words exist, keep decrypted message
+  }
+}
---
+++ isCantorSet.java
@@ -0,0 +1,64 @@
+// recursive quiz question for students on practice quiz 3 -> fractals!!!
+// my own creation >:D
+
+import java.util.*;
+
+public class isCantorSet {
+    public static void main(String[] args) {
+        List<int[]> sets = new ArrayList<>();
+        // sets.add(null); // IAE
+        // sets.add(new int[] {}); // IAE
+        sets.add(new int[] {0, 0, 0}); // false
+        sets.add(new int[] {1, 0, 1}); // true
+        sets.add(new int[] {1, 0, 1, 0, 0, 0, 1, 0, 1}); // true
+        sets.add(new int[] {1, 1, 1, 1, 1, 0, 1, 0, 1}); // false (0, 0, 1)
+        sets.add(new int[] {
+            1, 0, 1, 0, 0, 0, 1, 0, 1, 
+            1, 0, 1, 0, 0, 0, 1, 0, 1, 
+            1, 0, 1, 0, 0, 0, 1, 0, 1
+        }); // false
+        sets.add(new int[] {
+            1, 0, 1, 0, 0, 0, 1, 0, 1, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 
+            1, 0, 1, 0, 0, 0, 1, 0, 1
+        }); // true
+
+        for (int[] set : sets) {
+            System.out.println(Arrays.toString(set) + ": " + isCantorSet(set));
+        }
+    }
+
+    public static boolean isCantorSet(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("arr can't be null or empty!");
+        }
+        return isCantorSet(arr, 0, arr.length);
+    }
+
+    private static boolean isCantorSet(int[] arr, int start, int length) {
+        // (base case) checking the left and right of [1 0 1] since 
+        // the left and right are checked recursively
+        if (length == 1) {
+            return arr[start] == 1;
+        }
+        // subdivide into three segments
+        int divider = length/3;
+
+        // check left and right thirds recursively
+        // returns false if not cantor
+        if (!isCantorSet(arr, start, divider) || !isCantorSet(arr, start + 2 * divider, divider)) {
+            return false;
+        }
+        
+        // check middle third to ensure all are 0
+        for (int i = start + divider; i < start + 2 * divider; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+}
